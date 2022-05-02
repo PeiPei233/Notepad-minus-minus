044ed13f82d9bc704103fb46b4ebcb7f5eec9023
Bei7533 <104085779+Bei7533@users.noreply.github.com>
2022-05-02 21:57:04 +0800

    1
    11

--- edit.c
+++ edit.c
@@ -1,16 +1,47 @@
 /*
     主要包括复制 剪切 粘贴功能
-    这一部分应该要和Windows剪贴板进行交互，自行查找资料
+    这一部分应该要和Windows剪贴板进行交互
 */
-
+#include <stdio.h>
+#include <Windows.h>
 #include "global.h"
+#include "edit.h"
+
 
 /*
     根据传入的起始行列坐标与终止行列坐标进行复制
 */
-void copyText() {
+void copyText()
+{   
     RCNode startSelect = getSelectStartRC();
-    RCNode endSelect = getSelectEndRC();
+    RCNode endSelect = getSelectEndRC();    
+    char copystr[max];   //定义需要复制的文本
+    char curText[max];
+    strcpy(curText, allText());  //获取所有文本
+    char *cur = curText;  
+    int start, end;
+    start = numofFormerWords(startSelect);   //计算复制字符的起始位置，结束位置
+    end = numofFormerWords(endSelect);
+    cur += start;   // 指针指向选中复制文本开始处
+    strcpy(copystr, cur);   //得到选中复制文本
+    int length = end - start;
+    copystr[length] = '\0';		//设置复制文本结束符（文本计数从1开始）
+
+    //与系统剪切板进行交互
+    if (!OpenClipboard(NULL)|| !EmptyClipboard())
+    {   
+        printf("打开或清空剪切板出错！\n");
+        return ;   
+    }
+    HGLOBAL hMen;
+    hMen = GlobalAlloc(GMEM_MOVEABLE, ((strlen(copystr)+1)*sizeof(TCHAR)));    // 分配全局内存    
+    // 把数据拷贝到全局内存中
+    LPSTR lpStr = (LPSTR)GlobalLock(hMen);    // 先锁住内存区
+    memcpy(lpStr, copystr, ((strlen(copystr))*sizeof(TCHAR)));    // 内存复制
+    lpStr[strlen(copystr)] = (TCHAR)0;    // 设置字符结束符
+    GlobalUnlock(hMen);   // 释放锁
+    SetClipboardData(CF_TEXT, hMen);    // 把内存中的数据放到剪切板上
+    CloseClipboard();  //关闭剪切板
 }
 
 /*
@@ -19,6 +50,52 @@ void copyText() {
 void pasteText() {
     RCNode startSelect = getSelectStartRC();
     RCNode endSelect = getSelectEndRC();
+    int start, end, length;
+    start = numofFormerWords(startSelect);
+    end = numofFormerWords(endSelect);
+    length = end - start;  //计算要覆盖的字符的起始位置，终止位置，长度
+
+    //与系统剪切板交互读取其中内容并储存在pasteText当中
+    char pasteText[max];
+    if(!OpenClipboard(NULL))
+    {
+        printf("打开剪切板出错！\n");
+        return ;   
+    }
+    HGLOBAL hMem = GetClipboardData(CF_TEXT);
+    if(hMem)
+    {
+        //获取字符串
+        LPSTR lpStr = (LPSTR)GlobalLock(hMem);
+        if(lpStr)
+        {
+            strcpy(pasteText, lpStr);   //将剪切板的内容拷贝到字符串当中
+            GlobalUnlock(hMem);   //释放内存锁
+        }
+    }
+    //关闭剪切板
+    CloseClipboard();
+
+    //对原文本进行粘贴修改
+    char curText[max];  
+    strcpy(curText, allText());  //获取所有文本
+    char *cur = curText;
+    char behindText[max];   //定义被选中文本后的字符串
+    cur += (end+1);    //指针指向被选中文本的之后的第一个字符
+    strcpy(behindText, cur);
+    curText[start-1] = '\0';     //设置选择文本之前的字符串结束符
+    strcat(curText, pasteText);    //将选中文本之前字符串连接粘贴文本
+    strcat(curText, behindText);    //将粘贴后的文本与选中文本之后的字符串相连
+    
+    //将修改后文本输入缓存文件
+    FILE *currentFile;
+    if(!(currentFile = fopen("currentFile.txt","w")))
+    {
+        printf("打开错误");
+        return ;
+    }
+    fprintf(currentFile, "%s", curText);
+    fclose(currentFile);
 }
 
 /*
@@ -28,4 +105,29 @@ void pasteText() {
 void shearText() {
     RCNode startSelect = getSelectStartRC();
     RCNode endSelect = getSelectEndRC();
+    copyText();   //先对选中内容进行复制
+
+    //删除选中内容
+    int start, end, length;
+    start = numofFormerWords(startSelect);
+    end = numofFormerWords(endSelect);
+    length = end - start;
+    char curText[max];
+    strcpy(curText, allText());   //获取所有文本
+    char behindText[max];   //定义被选中文本后的字符
+    char *cur = curText;
+    cur += (end+1);    //指针指向被选中文本后的第一个字符
+    strcpy(behindText, cur);
+    curText[start-1] = '\0';    //设置选择文本之前的字符串结束符
+    strcat(curText, behindText);    //连接前后字符串，将中间删除
+    
+    //将修改后的文本输入
+    FILE *currentFile;  
+    if(!(currentFile = fopen("currentFile.txt","w")))
+    {
+        printf("打开错误");
+        return ;
+    }
+    fprintf(currentFile, "%s", curText);  
+    fclose(currentFile);
 }
--- global.h
+++ global.h
@@ -1,6 +1,10 @@
 #ifndef __GLOBAL_H__
 #define __GLOBAL_H__
 
+#include "callback.h"
+
+#define max 100000
+
 //行列坐标表示
 typedef struct {
     int row;  //行位置
@@ -42,22 +46,32 @@ RCNode getSelectEndRC();
 /*
     设置选择范围起始位置
 */
-void setSelectStartRC(RCNode newStartSelect);
+void setSelectStartLC(RCNode newStartSelect);
 
 /*
-    设置其实范围终止位置
+    设置选择范围终止位置
 */
-void setSelectEndRC(RCNode newEndSelect);
+void setSelectEndLC(RCNode newEndSelect);
 
 /*
     判断光标位置是否在窗口内
 */
-int cursorInWindow();
+int cursorInWindow(RCNode curCursor);
 
 /*
     更改光标位置使其能够展示在窗口内
     如果光标已经在窗口内则不调整
 */
 void setCursorInWindow();
+
+/*
+    实现将传入的行列数转化为总的字符数，方便在文件当中的读取
+*/
+int  numofFormerWords(RCNode curPosition);
+
+/*
+    将所有文本内容放入一个数组当中连续储存
+*/
+char* allText();
 
 #endif
